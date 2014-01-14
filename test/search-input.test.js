8123750c77d1ecd516c3032602cfe3af502cd978
garthdb <garthdb@gmail.com>
2014-01-13 20:44:54 -0800

    updated tests

--- test/search-input.test.js
+++ test/search-input.test.js
@@ -25,43 +25,43 @@ describe('Topcoat search-input', function() {
     'use strict';
 
     it('should output correct css', function() {
-        var actual = grunt.file.read('css/topcoat-search-input-desktop-dark.css');
-        var expected = grunt.file.read('test/expected/topcoat-search-input-desktop-dark.css');
+        var actual = grunt.file.read('css/desktop-dark.css');
+        var expected = grunt.file.read('test/expected/desktop-dark.css');
         assert.equal(actual, expected, 'should generate correct css');
     });
 
     it('should output correct css', function() {
-        var actual = grunt.file.read('css/topcoat-search-input-desktop-light.css');
-        var expected = grunt.file.read('test/expected/topcoat-search-input-desktop-light.css');
+        var actual = grunt.file.read('css/desktop-light.css');
+        var expected = grunt.file.read('test/expected/desktop-light.css');
         assert.equal(actual, expected, 'should generate correct css');
     });
 
     it('should output correct css', function() {
-        var actual = grunt.file.read('css/topcoat-search-input-mobile-dark.css');
-        var expected = grunt.file.read('test/expected/topcoat-search-input-mobile-dark.css');
+        var actual = grunt.file.read('css/mobile-dark.css');
+        var expected = grunt.file.read('test/expected/mobile-dark.css');
         assert.equal(actual, expected, 'should generate correct css');
     });
 
     it('should output correct css', function() {
-        var actual = grunt.file.read('css/topcoat-search-input-mobile-light.css');
-        var expected = grunt.file.read('test/expected/topcoat-search-input-mobile-light.css');
+        var actual = grunt.file.read('css/mobile-light.css');
+        var expected = grunt.file.read('test/expected/mobile-light.css');
         assert.equal(actual, expected, 'should generate correct css');
     });
 
     it('should not have any unrendered variables desktop dark', function() {
-        var actual = grunt.file.read('css/topcoat-search-input-desktop-dark.css');
+        var actual = grunt.file.read('css/desktop-dark.css');
         assert.equal(actual.match(/var-[a-z-]*[a-z]+/g), null, 'should not have missing vars');
     });
     it('should not have any unrendered variables desktop light', function() {
-        var actual = grunt.file.read('css/topcoat-search-input-desktop-light.css');
+        var actual = grunt.file.read('css/desktop-light.css');
         assert.equal(actual.match(/var-[a-z-]*[a-z]+/g), null, 'should not have missing vars');
     });
     it('should not have any unrendered variables mobile dark', function() {
-        var actual = grunt.file.read('css/topcoat-search-input-mobile-dark.css');
+        var actual = grunt.file.read('css/mobile-dark.css');
         assert.equal(actual.match(/var-[a-z-]*[a-z]+/g), null, 'should not have missing vars');
     });
     it('should not have any unrendered variables mobile light', function() {
-        var actual = grunt.file.read('css/topcoat-search-input-mobile-light.css');
+        var actual = grunt.file.read('css/mobile-light.css');
         assert.equal(actual.match(/var-[a-z-]*[a-z]+/g), null, 'should not have missing vars');
     });
 
